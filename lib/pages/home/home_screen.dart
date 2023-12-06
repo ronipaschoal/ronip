25d51cfcc465cf18a67b93bb0245bf3bffff1cd8
Roni Magozzo Paschoal <ronipaschoal@gmail.com>
2023-12-05 22:27:48 -0300

    feat: create anchor menu

--- lib/pages/home/home_screen.dart
+++ lib/pages/home/home_screen.dart
@@ -1,20 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:ronip/pages/home/sections/about_section.dart';
+import 'package:ronip/pages/home/sections/contact_section.dart';
 import 'package:ronip/pages/home/sections/home_section.dart';
+import 'package:ronip/pages/home/sections/work_section.dart';
 import 'package:ronip/ui/theme.dart';
 import 'package:ronip/ui/widgets/flutter_banner.dart';
 import 'package:ronip/ui/widgets/logo.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final _scrollController = ScrollController();
+
+  final _homeKey = GlobalKey();
+  final _aboutKey = GlobalKey();
+  final _workKey = GlobalKey();
+  final _contactKey = GlobalKey();
+
+  double _homePosition = 0.0;
+  double _aboutPosition = 0.0;
+  double _workPosition = 0.0;
+  double _contactPosition = 0.0;
 
   Future<void> _launchLinkedin() async {
     final url = Uri.parse('https://www.linkedin.com/in/roni-paschoal/');
     if (!await launchUrl(url)) {
       throw Exception('Could not launch $url');
     }
+  }
+
+  double _gtSectionPosition(GlobalKey key) {
+    final RenderObject? renderObject = key.currentContext?.findRenderObject();
+    if (renderObject is RenderBox) {
+      return renderObject.localToGlobal(Offset.zero).dy;
+    }
+    return 0;
+  }
+
+  void _selectSection(double position) {
+    _scrollController.animateTo(
+      position,
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _homePosition = _gtSectionPosition(_homeKey);
+      _aboutPosition = _gtSectionPosition(_aboutKey);
+      _workPosition = _gtSectionPosition(_workKey);
+      _contactPosition = _gtSectionPosition(_contactKey);
+    });
   }
 
   @override
@@ -24,68 +70,68 @@ class HomeScreen extends StatelessWidget {
       width: double.infinity,
       child: RpFlutterBanner(
         child: Scaffold(
-          body: NestedScrollView(
-            headerSliverBuilder: (context, innerBoxIsScrolled) => [
-              SliverOverlapAbsorber(
-                handle: NestedScrollView //
-                    .sliverOverlapAbsorberHandleFor(context),
-                sliver: SliverAppBar(
-                  title: const SizedBox(
-                    width: 48.0,
-                    child: RpLogo(),
+          extendBody: true,
+          extendBodyBehindAppBar: true,
+          appBar: AppBar(
+            title: const SizedBox(
+              width: 48.0,
+              child: RpLogo(),
+            ),
+            backgroundColor: Colors.transparent,
+            actions: [
+              TextButton(
+                child: const Text(
+                  'Home',
+                  style: TextStyle(color: RpTheme.witheColor),
+                ),
+                onPressed: () => _selectSection(_homePosition),
+              ),
+              TextButton(
+                child: const Text(
+                  'About',
+                  style: TextStyle(color: RpTheme.witheColor),
+                ),
+                onPressed: () => _selectSection(_aboutPosition),
+              ),
+              TextButton(
+                child: const Text(
+                  'Work',
+                  style: TextStyle(color: RpTheme.witheColor),
+                ),
+                onPressed: () => _selectSection(_workPosition),
+              ),
+              TextButton(
+                child: const Text(
+                  'Contact',
+                  style: TextStyle(color: RpTheme.witheColor),
+                ),
+                onPressed: () => _selectSection(_contactPosition),
+              ),
+              const SizedBox(width: 16.0),
+              IconButton(
+                onPressed: _launchLinkedin,
+                icon: SvgPicture.asset(
+                  'assets/images/logos/linkedin.svg',
+                  width: 16,
+                  height: 16,
+                  colorFilter: const ColorFilter.mode(
+                    RpTheme.witheColor,
+                    BlendMode.srcIn,
                   ),
-                  backgroundColor: RpTheme.backgroundColor,
-                  floating: true,
-                  snap: true,
-                  actions: [
-                    TextButton(
-                      child: const Text(
-                        'About',
-                        style: TextStyle(color: RpTheme.witheColor),
-                      ),
-                      onPressed: () {},
-                    ),
-                    const SizedBox(width: 16.0),
-                    TextButton(
-                      child: const Text(
-                        'Work',
-                        style: TextStyle(color: RpTheme.witheColor),
-                      ),
-                      onPressed: () {},
-                    ),
-                    const SizedBox(width: 16.0),
-                    TextButton(
-                      child: const Text(
-                        'Contact',
-                        style: TextStyle(color: RpTheme.witheColor),
-                      ),
-                      onPressed: () {},
-                    ),
-                    const SizedBox(width: 16.0),
-                    IconButton(
-                      onPressed: _launchLinkedin,
-                      icon: SvgPicture.asset(
-                        'assets/images/logos/linkedin.svg',
-                        width: 16,
-                        height: 16,
-                        colorFilter: const ColorFilter.mode(
-                          RpTheme.witheColor,
-                          BlendMode.srcIn,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 48.0),
-                  ],
                 ),
               ),
+              const SizedBox(width: 48.0),
             ],
-            body: const SingleChildScrollView(
-              child: Column(
-                children: [
-                  HomeSection(),
-                  AboutSection(),
-                ],
-              ),
+          ),
+          body: SingleChildScrollView(
+            controller: _scrollController,
+            child: Column(
+              children: [
+                HomeSection(key: _homeKey),
+                AboutSection(key: _aboutKey),
+                WorkSection(key: _workKey),
+                ContactSection(key: _contactKey),
+              ],
             ),
           ),
         ),
---
+++ lib/pages/home/sections/work_section.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:ronip/ui/theme.dart';
+
+class WorkSection extends StatelessWidget {
+  const WorkSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.sizeOf(context).height,
+      width: MediaQuery.sizeOf(context).width,
+      child: Container(
+        color: RpTheme.backgroundColor,
+        child: const Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SelectableText(
+              'Work',
+              semanticsLabel: 'Work',
+              style: TextStyle(
+                color: RpTheme.witheColor,
+                fontSize: 42.0,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
