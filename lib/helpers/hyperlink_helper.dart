664f608c8e1b3d8276e996b132eb9b173d6c722a
Roni Magozzo Paschoal <ronipaschoal@gmail.com>
2024-06-02 21:12:40 -0300

    feat: add mail link

--- lib/helpers/hyperlink_helper.dart
+++ lib/helpers/hyperlink_helper.dart
@@ -7,4 +7,11 @@ sealed class HyperlinkHelper {
       throw Exception('Could not launch $url');
     }
   }
+
+  static void mail(String link) async {
+    final url = Uri.parse(link);
+    if (!await launchUrl(url)) {
+      throw Exception('Could not launch $url');
+    }
+  }
 }
--- lib/pages/home/sections/contact_section.dart
+++ lib/pages/home/sections/contact_section.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:ronip/helpers/hyperlink_helper.dart';
 import 'package:ronip/ui/theme.dart';
 
 class ContactSection extends StatelessWidget {
@@ -20,9 +21,17 @@ class ContactSection extends StatelessWidget {
             style: const TextStyle(fontSize: RpTheme.fontSizeLarge),
           ),
           RpTheme.spacerLarge,
-          const SelectableText(
-            'contato@ronip.dev',
-            semanticsLabel: 'contato@ronip.dev',
+          TextButton(
+            child: const Text(
+              'contato@ronip.dev',
+              style: TextStyle(
+                color: RpTheme.textColor,
+                fontSize: RpTheme.fontSizeRegular,
+              ),
+            ),
+            onPressed: () => HyperlinkHelper.mail(
+              'mailto:contato@ronip.dev?subject=Website contact!',
+            ),
           ),
         ],
       ),
