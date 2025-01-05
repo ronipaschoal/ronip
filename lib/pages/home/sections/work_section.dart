2442e81574219c1812e3547aa6aa33dcdfdc0999
Roni Magozzo Paschoal <ronipaschoal@gmail.com>
2025-01-05 13:25:12 -0300

    refactor: adjust works section

--- lib/pages/home/sections/work_section.dart
+++ lib/pages/home/sections/work_section.dart
@@ -11,27 +11,34 @@ class WorkSection extends StatelessWidget {
 
   static const _workList = [
     {
-      'title': 'Angular',
+      'title': 'Roni Paschoal (V1)',
+      'description': 'Site anterior desenvolvido em AngularJS.',
       'url': 'https://angular.ronipaschoal.com.br/',
       'image': 'assets/images/photos/site-roni-paschoal-angularjs.png',
     },
     {
-      'title': 'Flutter, Eremita do Iceberg',
+      'title': 'O Eremita do Iceberg, Flutter',
+      'description':
+          'Estudo de animações nativas e gerenciamento de estados Bloc/Cubit, em Flutter.',
       'url': 'https://eremitaflutter.ronipaschoal.com.br/',
       'image': 'assets/images/photos/flutter-o-eremita-do-iceberg.png',
     },
     {
       'title': 'Reali Plásticos',
+      'description':
+          'Desenvolvimento em PHP, criação das imagens em 3d, UX e SEO do site institucional da empresa.',
       'url': 'https://www.realiplasticos.com.br/',
       'image': 'assets/images/photos/site-reali-plasticos.png',
     },
     {
-      'title': 'App Flutter, Minha Comanda Eletrônica',
+      'title': 'Minha Comanda Eletrônica, App Flutter',
+      'description':
+          'Participação na concepção e desenvolvimento em Flutter (Android, IOS, Cielo, Rede e PagSeguro), MVVM e Bloc/Cubit.',
       'url':
           'https://play.google.com/store/apps/details?id=com.totvs.thex.minhacomanda',
       'urlApple':
           'https://apps.apple.com/br/app/minha-comanda-eletr%C3%B4nica/id6474201107',
-      'image': 'assets/images/photos/minha-comanda-digital.png',
+      'image': 'assets/images/photos/minha-comanda.png',
     },
   ];
 
@@ -53,26 +60,38 @@ class WorkSection extends StatelessWidget {
             itemCount: _workList.length,
             itemBuilder: (ctx, index) {
               final work = _workList[index];
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    onPressed: () => (isIOS || isMacOS) &&
-                            work['urlApple']?.isNotEmpty != null
-                        ? HyperlinkHelper.targetBlank(work['urlApple']!)
-                        : HyperlinkHelper.targetBlank(work['url']!),
-                    icon: Column(
-                      children: [
-                        RpImageWidget(
-                          asset: work['image']!,
-                          width: 700.0,
-                        ),
-                        Text(work['title']!),
-                      ],
+              return Center(
+                child: Stack(
+                  alignment: Alignment.bottomRight,
+                  children: [
+                    IconButton(
+                      onPressed: () => (isIOS || isMacOS) &&
+                              work['urlApple']?.isNotEmpty != null
+                          ? HyperlinkHelper.targetBlank(work['urlApple']!)
+                          : HyperlinkHelper.targetBlank(work['url']!),
+                      icon: RpImageWidget(
+                        asset: work['image']!,
+                        width: 700.0,
+                      ),
                     ),
-                  ),
-                ],
+                    Positioned(
+                      bottom: 16.0,
+                      right: 16.0,
+                      child: Container(
+                        width: 320.0,
+                        padding: const EdgeInsets.all(16.0),
+                        color: RpTheme.backgroundColor.withAlpha(140),
+                        child: Text(
+                          '${work['title']!}\n\n${work['description']!}',
+                          style: const TextStyle(
+                            color: RpTheme.textHighlightColor,
+                            fontSize: 14.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               );
             },
             separatorBuilder: (_, __) => RpTheme.spacerLarge,
